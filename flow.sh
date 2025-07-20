67a401774f488de221fd31da159e06bc8ced4dab
Avi Avni <avi.avni@gmail.com>
2025-07-20 18:05:11 +0300

    fix script for linux

--- flow.sh
+++ flow.sh
@@ -1,1 +1,6 @@
-RLTest -f flow_tests_done.txt --module target/debug/libfalkordb.dylib --no-progress --parallelism 8 --clear-logs --log-dir tests/flow/logs
+cargo build
+if [ Darwin == "$(uname -s)" ]; then
+  RLTest -f flow_tests_done.txt --module target/debug/libfalkordb.dylib --no-progress --parallelism 8 --clear-logs --log-dir tests/flow/logs
+else
+  RLTest -f flow_tests_done.txt --module target/debug/libfalkordb.so --no-progress --parallelism 8 --clear-logs --log-dir tests/flow/logs
+fi
