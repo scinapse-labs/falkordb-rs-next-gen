284714a64655021817af08fe1ef474d439c7368d
AviAvni <avi.avni@gmail.com>
2025-07-20 18:33:27 +0300

    fix bash

--- flow.sh
+++ flow.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 cargo build
-if [ Darwin == "$(uname -s)" ]; then
+if [[ "$(uname -s)" == "Darwin" ]]; then
   RLTest -f flow_tests_done.txt --module target/debug/libfalkordb.dylib --no-progress --parallelism 8 --clear-logs --log-dir tests/flow/logs
 else
   RLTest -f flow_tests_done.txt --module target/debug/libfalkordb.so --no-progress --parallelism 8 --clear-logs --log-dir tests/flow/logs
