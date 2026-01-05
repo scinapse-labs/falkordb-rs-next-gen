5a5c1acaac9795a1a0cdd5a035b24bca44faa3af
Barak Bar Orion <barak.bar@gmail.com>
2026-01-05 12:53:54 +0200

    fix expected lat to be the same as the given lat

--- tests/flow/test_function_calls.py
+++ tests/flow/test_function_calls.py
@@ -567,15 +567,15 @@ class testFunctionCallsFlow(FlowTestsBase):
         query = """RETURN toJSON(point({ longitude: 167.697555, latitude: 0.402313 }))"""
         actual_result = self.graph.query(query)
         parsed = json.loads(actual_result.result_set[0][0])
-        self.env.assertEquals(parsed, {"crs": "wgs-84", "latitude": 0.402313, "longitude": 167.697556, "height": None})
+        self.env.assertEquals(parsed, {"crs": "wgs-84", "latitude": 0.402313, "longitude": 167.697555, "height": None})
 
-    ## Memory should be freed properly when the key values are heap-allocated.
-    #def test18_allocated_keys(self):
-    #    query = """UNWIND ['str1', 'str1', 'str2', 'str1'] AS key UNWIND [1, 2, 3] as agg RETURN toUpper(key) AS key, collect(DISTINCT agg) ORDER BY key"""
-    #    actual_result = self.graph.query(query)
-    #    expected_result = [['STR1', [1, 2, 3]],
-    #                       ['STR2', [1, 2, 3]]]
-    #    self.env.assertEquals(actual_result.result_set, expected_result)
+    # Memory should be freed properly when the key values are heap-allocated.
+    def test18_allocated_keys(self):
+        query = """UNWIND ['str1', 'str1', 'str2', 'str1'] AS key UNWIND [1, 2, 3] as agg RETURN toUpper(key) AS key, collect(DISTINCT agg) ORDER BY key"""
+        actual_result = self.graph.query(query)
+        expected_result = [['STR1', [1, 2, 3]],
+                           ['STR2', [1, 2, 3]]]
+        self.env.assertEquals(actual_result.result_set, expected_result)
 
     #def test19_has_labels(self):
     #    # Test existing label
