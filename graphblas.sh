13d947c2fd2529a64d6e9fc4e8bc528af052d337
Avi Avni <avi.avni@gmail.com>
2026-02-16 09:21:27 +0200

    update

--- graphblas.sh
+++ graphblas.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone --branch v10.3.0 --single-branch https://github.com/DrTimothyAldenDavis/GraphBLAS.git
+git clone --branch v10.3.1 --single-branch https://github.com/DrTimothyAldenDavis/GraphBLAS.git
 cd GraphBLAS
 make static CMAKE_OPTIONS='-DGRAPHBLAS_COMPACT=1 -DCMAKE_POSITION_INDEPENDENT_CODE=on' CC=clang CXX=clang++
 sudo make install
